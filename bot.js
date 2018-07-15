c271b1a22f68e85af7c457c0d2df5848ebfbd75b
PokerDz <41197776+PokerDz@users.noreply.github.com>
2018-07-15 16:27:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-const Discord = require('discord.js');
+
 const client = new Discord.Client();
 client.on('ready', () => {
   client.user.setGame('*help | *invite','https://www.twitch.tv/peery13');
