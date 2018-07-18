092b594a48fd84d1de80e3d46504d955162eff93
PokerDz <41197776+PokerDz@users.noreply.github.com>
2018-07-18 16:34:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,9 +27,6 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-const client = new Discord.Client();
-const prefix = '!'
- 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
