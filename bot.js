1b240632079c48c6ee3be6c971732b64e9626cd7
PokerDz <41197776+PokerDz@users.noreply.github.com>
2018-07-18 17:02:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,4 +49,81 @@ client.on('message', message => {
 message.channel.sendMessage('**الرجاء الانتظار ريث ما يتم صناعه الرتب **')
 }
 });
+client.on("message", message => {
+ if (message.content === "امر البوت الي تبيه") {
+  const embed = new Discord.RichEmbed() 
+      .setColor("#ffff00")
+      .setThumbnail(message.author.avatarURL)
+      .setDescription(`
+
+***__وصف عن البوت__***
+**
+وصف عن بوتك هنا
+
+**
+
+        ***__General orders__***
+**
+『f!id / معلومات عن حسابك』
+『f!draw / يكرر كلامك في صوره』
+『f!embed / يكرر كلامك بمبيد』
+『f!roles / يعرض لك الرتب و عددها』
+『f!rooms / يعرض لك الرومات وعددها』
+『f!server / معلومات عن السيرفر』
+『f!animal / يعطيك صور حيوانات』
+『f!image / يعرض صوره السيرفر』
+『f!avatar / يعرض صورتك او صوره شخص』
+『f!time / يعرض لك الوقت』
+『f!date / يعرض لك التاريخ』
+『f!dt / يعرض الوقت في الامارات و مكه المكرمه و مصر و التاريخ』
+**
+
+        ***__Bot orders__***
+**
+『f!ping / يعرض لك سرعه اتصال البوت』
+『f!uptime / يعرض لك صار للبوت كم شغال』
+『f!support / سيرفر الدعم القني و المساعده』
+『f!invite / اضافه البوت』
+『f!members / حاله الاعضاء』
+『f!bot / معلومات عن البوت』
+**
+
+        ***__Administrative Orders__***
+**
+『f!kick / كيك』
+『f!ban / بان』
+『f!mute /ميوت』
+『f!unmute /فك الميوت』
+『f!mutechannel /قفل الشات』
+『f!unmutechannel /فتح الشات』
+『f!add.r / اضافه رتبه』
+『f!delet / مسح روم』
+『f!color 50 /انشاء 50 لون』
+『f!color 100/انشاء 100 لون』
+『f!color 140/انشاء 140 لوم』
+『f!ct /انشاء روم كتابي』4
+『f!cv /انشاء روم صوتي』
+『f!bc /برودكاست』
+**
+
+
+        ***__Games orders__***
+ **       
+『f!لعبه صراحه / صراحه』
+『f!لعبه مريم / مريم』
+『f!لعبه خواطر / خواطر』
+『f!يعطيك شعر عن الحب / حب』
+『f!يخيرك بين شي وشي / لو خيروك』
+『f!يعطيك عقاب و لازم تنفذه / عقاب』
+『f!لعبه اسئله / كت تويت』
+**
+   
+        
+`)
+
+
+message.author.sendEmbed(embed)
+
+}
+}); 
 client.login(process.env.BOT_TOKEN);
