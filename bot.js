76e27065e849c8366a44921f272e1f1c6cb22419
PokerDz <41197776+PokerDz@users.noreply.github.com>
2018-07-21 11:44:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`SR Server`,"http://twitch.tv/S-F")
+client.user.setGame(`Bot By Poker`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
