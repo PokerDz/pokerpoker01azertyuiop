a687530fde36777b9bb0f31fc20aa253a59fbb7b
PokerDz <41197776+PokerDz@users.noreply.github.com>
2018-12-27 08:14:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,56 +27,7 @@ client.user.setGame(`By Poker`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-client.on('message', message => {
-    if (message.content.startsWith("SR-avatar")) {
-        if(!message.channel.guild) return message.reply('** This command only for servers **');
-   
-        var mentionned = message.mentions.users.first();
-    var x5bzm;
-      if(mentionned){
-          var x5bzm = mentionned;
-      } else {
-          var x5bzm = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor(0xd3d0c4)
-        .setImage(`${x5bzm.avatarURL}`)
-           .setFooter(`©  Dreams™ `)
-      message.channel.sendEmbed(embed);
-    }
-});
-const Discord = require('discord.js');
-const db = require('quick.db');
-const client = new Discord.Client();   
-const giphy = require('giphy-api')();    
-const googl = require('goo.gl'); 
-const translate = require('google-translate-api'); 
-const fs = require("fs");      
-const getYoutubeID = require('get-youtube-id'); 
-const moment = require("moment");  
-const { Client, Util } = require('discord.js');  
-const UserBlocked = new Set();   
-const jimp = require('jimp');   
-const math = require('math-expression-evaluator'); 
-const stripIndents = require('common-tags').stripIndents;
-const figlet = require('figlet');
-const google = require('google-it'); 
-const queue = new Map();
-const zalgo = require('zalgolize');   
-const fetchVideoInfo = require('youtube-info');
-const YouTube = require('simple-youtube-api');
-const ytdl = require('ytdl-core');
-const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
-const sql = require("sqlite");
- const dateFormat = require('dateformat');
- const pretty = require('pretty-ms') 
-,ti={}  
-,spee={};
- 
-
-
-client.on("message", message => {
+ client.on("message", message => {
 	var prefix = "-";
  if (message.content === "-help**)(") {
   const embed = new Discord.RichEmbed() 
