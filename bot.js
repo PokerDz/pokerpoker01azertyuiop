5d06b47cd4ca3b487b5f03b5e000aec19ba5a7ea
PokerDz <41197776+PokerDz@users.noreply.github.com>
2019-01-03 15:29:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ client.on('ready', () => {
 
 client.on('ready', () => {
 
-    client.user.setActivity("$help | Beta V.0.0.1",{type: 'Streaming'})
+    client.user.setActivity("By Poker | Beta V.0.0.1",{type: 'Streaming'})
 
 });
 
@@ -1935,4 +1935,33 @@ if(message.content == prefix+"عواصم"){
                                 
                                     serverQueue.textChannel.send(`**${song.title}**, is now playing!`);
                                 }
+//Best Rainbow Bot .
+client.on('message', message => {//new msg event
+if(!message.channel.guild) return;
+  if(message.content.startsWith(prefix + 'set')) {//to create the rainbow role
+	  let role = message.guild.roles.find('name', 'Rainbow bot.')
+    if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
+  //start of create role 
+  if(!role){
+    rainbow =  message.guild.createRole({
+   name: "Rainbow bot.",//the role will create name
+   color: "#000000",//the default color
+   permissions:[]//the permissions
+ //end of create role
+})
+
+}
+message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
+}})
+
+client.on('ready', () => {//new ready event
+  setInterval(function(){
+      client.guilds.forEach(g => {
+                  var role = g.roles.find('name', 'Rainbow bot.');//rainbow role name
+                  if (role) {
+                      role.edit({color : "RANDOM"});
+                  };
+      });
+  }, 5000);//the rainbow time
+})
 client.login(process.env.BOT_TOKEN); 
