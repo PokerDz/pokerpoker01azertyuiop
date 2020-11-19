6a887856db0951da082669f6b45f8ecc1e182f1e
PokerDz <41197776+PokerDz@users.noreply.github.com>
2020-11-19 10:16:24 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,7 +1,3 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '.'
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`By ｍｏｏｈａ`)
